56adb70e3c907de038b191de206acb982a840bb1
Everton Soares <everton_dev@hotmail.com>
2025-07-06 18:03:51 -0300

    cards

--- resources/views/components/card.blade.php
+++ resources/views/components/card.blade.php
@@ -10,13 +10,15 @@
         </h5>
     </div>
     <div class="card-division w-100 bg-primary my-2" style="height:.1px;"></div>
-    <div class="my-card-body text-secondary" style="height: 230px; font-size:15px;">
+    <div class="my-card-body text-secondary" style="height: 230px; font-size: .9rem; overflow: hidden; text-overflow: ellipsis; white-space: normal;">
         {{ $slot }}
     </div>
+
     @if (!empty($technologies))
         <div class="my-2 small d-none d-lg-block">
             @foreach ($technologies as $technology)
-                <span class="btn btn-sm mt-2 rounded-pill" style="background-color:#0659c5"><strong class="text-white"> {{ $technology }}</strong></span>
+                <span class="btn btn-sm mt-2 rounded-pill" style="background-color:#0659c5"><strong class="text-white">
+                        {{ $technology }}</strong></span>
             @endforeach
         </div>
     @endif
