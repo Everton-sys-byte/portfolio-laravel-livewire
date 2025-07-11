b7d2136a66c25207a4aeaf6de83dee71b6489d3a
Everton Soares <everton_dev@hotmail.com>
2025-07-11 13:48:43 -0300

    email de contato

--- app/Mail/ContactFormMail.php
+++ app/Mail/ContactFormMail.php
@@ -28,7 +28,7 @@ class ContactFormMail extends Mailable
     public function envelope(): Envelope
     {
         return new Envelope(
-            subject: 'Contact Form Mail',
+            subject: 'Nova mensagem do portfólio',
         );
     }
 
